85de32f2d71c4b72a848e77dc5c7dbc1ac0b3f90
Daniel Huber <daniel.huber@bullhorn.com>
2022-02-03 12:30:16 -0500

    Added invalid file check

--- src/App.js
+++ src/App.js
@@ -7,37 +7,67 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contents: []
+      contents: [],
+      showError: false
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(data) {
-    console.log('data ' + data);
     let reader = new FileReader();
 
-    reader.onload = (e) => {
-      console.log('e ' + e);
-      const contents = e.target.result;
+    reader.onload = (file) => {
+      console.log('file ' + file);
+      const contents = file.target.result;
       const contentArray = contents.split('\r\n').filter(row => {
         return row && row.length > 0;
       });
 
-      this.setState({
-        contents: contentArray
-      });
+      this.checkDataAndSetState(contentArray);
     };
 
     reader.onload = reader.onload.bind(this);
     reader.readAsBinaryString(data.target.files[0]);
   }
 
+  checkDataAndSetState(contentArray) {
+    const HEADER_ROW = 'FirstName,LastName,Email,Phone';
+
+    if (contentArray && contentArray.length > 0) {
+      const existingHeaderRow = contentArray[0];
+
+      if (HEADER_ROW === existingHeaderRow) {
+        this.setState({
+          showError: false,
+          contents: contentArray
+        });
+      } else {
+        this.setState({
+          showError: true
+        });
+      }
+
+    }
+  }
+
   render() {
+    let error = '';
+    const controlKey = this.state.contents.length + this.state.showError;
+
+    if (this.state.showError) {
+      error =
+        <div>
+          <h4>Invalid file header, please enter another file with the valid file header</h4>
+        </div>;
+    }
+
     return (
       <div className="App">
         <FileInput handleChange={this.handleChange}/>
         <p/>
-        <Controls key={this.state.contents.length} disabled={this.state.contents.length === 0} contents={this.state.contents}/>
+          {error}
+        <p/>
+        <Controls key={controlKey} disabled={this.state.showError || this.state.contents.length === 0} contents={this.state.contents}/>
       </div>
     )
   }
